96680279e04a564e36836897a2abeb5c73ca82ef
Guilherme Torres Melo <guigi1005@gmail.com>
2024-02-02 10:59:02 -0300

    Começo

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+          main: '#ffffff',
+        },
+        secondary: {
+          main: '#090a0c',
+        },
+        tertiary: {
+          main: '#6d7e88',
+        }
+      },
+})
